ec352c9b9f6e14d90770750b629cf93ef23e3d48
Diego Anunciacao <diegoeliasanunciacao@gmail.com>
2024-11-30 01:16:38 +0000

    logout

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -27,10 +27,10 @@ Devise.setup do |config|
   config.mailer_sender = 'please-change-me-at-config-initializers-devise@example.com'
 
   config.omniauth :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET'], {
-  scope: 'email,profile',
-  prompt: 'select_account',
-  redirect_uri: Rails.env.production? ? 'https://seu-app.herokuapp.com/users/auth/google_oauth2/callback' : 'http://localhost:3000/users/auth/google_oauth2/callback'
-}
+    scope: 'email,profile',
+    prompt: 'select_account',
+    redirect_uri: 'http://localhost:3000/users/auth/google_oauth2/callback' # Substitua pelo seu redirect_uri
+  }
 
 
   # Configure the class responsible to send e-mails.

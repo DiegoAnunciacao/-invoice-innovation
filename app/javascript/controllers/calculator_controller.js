10705350f1d2b4df37ecc6299f280c6c24ab5081
Diego Anunciacao <diegoeliasanunciacao@gmail.com>
2024-11-30 20:03:35 +0000

    sum of fields invoice

---
+++ app/javascript/controllers/calculator_controller.js
@@ -0,0 +1,35 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="calculator"
+export default class extends Controller {
+  static targets = ["amount", "subtotal", "total", "vat", "remove"]
+
+  connect() {
+    this.calculateSubtotal()
+  }
+
+  calculateSubtotal() {
+    let subtotal = 0
+    this.amountTargets.forEach( (amount)=> {
+      subtotal += parseFloat(amount.value) || 0
+
+    })
+
+    const vat = parseFloat(this.vatTarget.value) || 0
+
+    const total = subtotal + (subtotal * vat / 100)
+
+    this.subtotalTarget.innerText = subtotal.toFixed(2)
+    this.totalTarget.innerText = total.toFixed(2)
+  }
+
+  updateOnInput() {
+    this.calculateSubtotal()
+  }
+
+  updateAfterRemove(event) {
+    event.preventDefault()
+    event.target.closest(".service-field").remove()
+    this.calculateSubtotal()
+  }
+}

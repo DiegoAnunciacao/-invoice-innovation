3efba77d9ae8991f7a6a6f4a0d201b9b828cdd22
Diego Anunciacao <diegoeliasanunciacao@gmail.com>
2025-01-09 18:38:24 +0000

    profile updates

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :authenticate_user!
   include Pundit::Authorization
 
@@ -12,6 +13,13 @@ class ApplicationController < ActionController::Base
   #   flash[:alert] = "You are not authorized to perform this action."
   #   redirect_to(root_path)
   # end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :phone, :company_name, :company_address, :company_number])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :phone, :company_name, :company_address, :company_number])
+  end
 
   private
 
